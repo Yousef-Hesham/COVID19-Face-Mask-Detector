087ee834ac35307164eea160b438704089f74c8d
Yousef Hesham <Yousef.hesham.samir@gmail.com>
2020-05-21 17:08:34 +0200

    V2.0 of detect_mask_video.py
    New version that includes using an input video instead of a video stream of the camera.

--- detect_mask_video.py
+++ detect_mask_video.py
@@ -92,42 +92,49 @@ print("[INFO] loading face mask detector model...")
 maskNet = load_model(args["model"])
 # initialize the video stream and allow the camera sensor to warm up
 print("[INFO] starting video stream...")
-vs = VideoStream(src=0).start()
-time.sleep(2.0)
 
 
-# loop over the frames from the video stream
-while True:
+cap = cv2.VideoCapture("Videos/vid1.mp4")
+
+
+fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (700,700))
+
+while(cap.isOpened()):
 	# grab the frame from the threaded video stream and resize it
 	# to have a maximum width of 400 pixels
-	frame = vs.read()
-	frame = imutils.resize(frame, width=400)
+    ret, current_frame = cap.read() #cap.read returns a boolean, and an image [frame] currently displayed.
+    if not ret:
+        break
+    current_frame = imutils.resize(current_frame, width=700)
 	# detect faces in the frame and determine if they are wearing a
 	# face mask or not
-	(locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
-    # loop over the detected face locations and their corresponding
+    (locs, preds) = detect_and_predict_mask(current_frame, faceNet, maskNet)
+
+	# loop over the detected face locations and their corresponding
 	# locations
-	for (box, pred) in zip(locs, preds):
-		# unpack the bounding box and predictions
-		(startX, startY, endX, endY) = box
-		(mask, withoutMask) = pred
-		# determine the class label and color we'll use to draw
-		# the bounding box and text
-		label = "Mask" if mask > withoutMask else "No Mask"
-		color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
-		# include the probability in the label
-		label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
-		# display the label and bounding box rectangle on the output
+    for (box, pred) in zip(locs, preds):
+        # unpack the bounding box and predictions
+        (startX, startY, endX, endY) = box
+        (mask, withoutMask) = pred
+        # determine the class label and color we'll use to draw
+        # the bounding box and text
+        label = "Mask" if mask > withoutMask else "No Mask"
+        color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+        # include the probability in the label
+        label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+        # display the label and bounding box rectangle on the output
 		# frame
-		cv2.putText(frame, label, (startX, startY - 10),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-		cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
-        # show the output frame
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
-# do a bit of cleanup
+        cv2.putText(current_frame, label, (startX, startY - 10),
+        	cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+        cv2.rectangle(current_frame, (startX, startY), (endX, endY), color, 2)
+        # show the output frame	
+    cv2.imshow('processesed.', current_frame)
+    out.write(current_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        break
+
+cap.release()
+out.release()
 cv2.destroyAllWindows()
-vs.stop()
